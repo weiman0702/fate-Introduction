c45b85ce92bf51a7bf1eeaafda9de6382ee5e470
User19 <user19@user19-imac.local>
2018-10-11 15:24:19 +0800

    Initial Commit

---
+++ fate Introduction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fate Introduction
+//
+//  Created by User19 on 2018/10/11.
+//  Copyright © 2018 laura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
