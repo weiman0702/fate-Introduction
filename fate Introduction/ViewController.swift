06d0f8cef3d3dfed1ed9817f47ba0edb70844b1e
User19 <user19@user19-imac.local>
2018-10-13 23:48:12 +0800

    first

--- fate Introduction/ViewController.swift
+++ fate Introduction/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var GifView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        GifView.loadGif(name: "32ZjD4")
     }
 
 
